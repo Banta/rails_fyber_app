59d63468ad496a41f3c4bf93972009c2b8933589
Bernard Banta <banta.bernard@gmail.com>
2014-08-08 14:58:41 +0300

    Created fiber_offer_api_spec test stub

---
+++ spec/lib/fiber_offer_api_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'FiberOfferApi' do
+
+  before(:each) do
+    @appid = 157
+    @device_id = '2b6f0cc904d137be2e1730235f5664094b831186'
+    @locale = 'de'
+    @ip = '109.235.143.113'# (german IP)'
+    @offer_types = 112
+    @api_Key = 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
+  end
+
+  it 'FiberOfferApi#new()' do
+    #TODO @Banta
+  end
+end
+
