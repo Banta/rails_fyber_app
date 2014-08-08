1dfad94566250cc417a9c170d52582a89c9012b2
Bernard Banta <banta.bernard@gmail.com>
2014-08-09 01:33:17 +0300

    Done implementing FiberOfferApi

--- lib/fiber_offer_api.rb
+++ lib/fiber_offer_api.rb
@@ -1,5 +1,41 @@
 class FiberOfferApi
-  def initialize
-     # TODO @Banta
+  attr_accessor :success, :code, :body
+
+  def initialize(uid, pub0, page)
+    @fiber_api_key = Rails.application.secrets.fiber_api_key
+    @fiber_device_id = Rails.application.secrets.fiber_device_id
+
+    @params = {
+        appid: 157,
+        uid: uid,
+        ip: '109.235.143.113',
+        locale: 'de',
+        device_id: @fiber_device_id,
+        ps_time: Time.now.to_i,
+        pub0: pub0,
+        page: page,
+        timestamp: Time.now.to_i,
+        offer_types: 112
+    }
+
+    response = Typhoeus::Request.get("api.sponsorpay.com/feed/v1/offers.json",
+        method: :get,
+        params: @params.merge({hashkey: getHashKey}),
+    )
+
+    @success = response.success?
+    if response.success?
+      @code = response.code
+      @body = JSON.parse(response.body)
+    end
+  end
+
+  def getHashKey
+    sorted_stringified_params = (@params.sort.inject([]) do |accu, (key, value)|
+      accu << key.to_s + '=' + value.to_s
+      accu
+    end).join('&') + "&#{@fiber_api_key}"
+
+    Digest::SHA1.hexdigest(sorted_stringified_params).to_s
   end
 end
