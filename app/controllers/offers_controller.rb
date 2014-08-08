2e04583e4d02282ac96693692fe6b6378141ad4c
Bernard Banta <banta.bernard@gmail.com>
2014-08-09 02:51:16 +0300

    Check offers form with validations is done

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,5 +3,25 @@ class OffersController < ApplicationController
   end
 
   def offers
+    require 'fiber_offer_api'
+
+    if params[:uid].blank? || params[:pub0].blank? || params[:page].blank?
+      arr = []
+      if params[:uid].blank?
+        arr << "UID is required"
+      end
+
+      if params[:pub0].blank?
+        arr << "Pub0 is required"
+      end
+
+      if params[:page].blank?
+        arr << "Page is required"
+      end
+
+      flash[:alert] = arr.to_sentence
+      redirect_to offers_check_path and return
+    end
+
   end
 end
