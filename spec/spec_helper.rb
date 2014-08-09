e27ceb60e7ef38f165c24c20a1c263038b867c67
Bernard Banta <banta.bernard@gmail.com>
2014-08-09 20:48:19 +0300

    Environment should be 'test'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+ENV["RAILS_ENV"] ||= 'test'
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'pry' if ENV['APP_ENV'] == 'debug' # add `binding.pry` wherever you need to debug
 require File.expand_path("../../config/environment", __FILE__)
