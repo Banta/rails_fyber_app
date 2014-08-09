1bf13a709139e33b05e6d9f83c944a335d12bba8
Bernard Banta <banta.bernard@gmail.com>
2014-08-09 20:49:04 +0300

    Added 4 tests for fiber_offer_api lib

--- spec/lib/fiber_offer_api_spec.rb
+++ spec/lib/fiber_offer_api_spec.rb
@@ -11,8 +11,30 @@ describe 'FiberOfferApi' do
     @api_Key = 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
   end
 
-  it 'FiberOfferApi#new()' do
-    #TODO @Banta
+  describe 'FiberOfferApi#new()' do
+    context "response" do
+       it "should be success if api key and other things are correct" do
+         response = FiberOfferApi.new(1, 'campaigns2', 1)
+         expect(response.success).to be true
+       end
+
+       it "should return a json body" do
+         response = FiberOfferApi.new(1, 'campaigns2', 1)
+         expect(response.body.class).to be Hash
+       end
+
+       it "should return 401 code if api key is wrong" do
+         Rails.application.secrets.fiber_api_key = "***ue37248237t####"
+         response = FiberOfferApi.new(1, 'campaigns2', 1)
+         expect(response.code).to be 401
+       end
+
+       it "should return 'ERROR_INVALID_HASHKEY' code if api key is wrong" do
+         Rails.application.secrets.fiber_api_key = "***ue37248237t####"
+         response = FiberOfferApi.new(1, 'campaigns2', 1)
+         expect(response.body['code']).to eql 'ERROR_INVALID_HASHKEY'
+       end
+    end
   end
 end
 
